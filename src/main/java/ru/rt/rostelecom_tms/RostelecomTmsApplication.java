59e90f8114e31e4070fab077f979aec7f52bdf95
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2026-02-20 14:05:42 +0300

    initial commit

---
+++ src/main/java/ru/rt/rostelecom_tms/RostelecomTmsApplication.java
@@ -0,0 +1,13 @@
+package ru.rt.rostelecom_tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RostelecomTmsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RostelecomTmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rt/rostelecom_tms/RostelecomTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rt.rostelecom_tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RostelecomTmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
