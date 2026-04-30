0cfaf52f9ff47a3be0b347d38ee1dde1fb430982
Kirill Luzin <luzin2147@gmail.com>
2026-04-30 16:36:54 +0300

    refactor: remove project id from registration

--- src/main/java/ru/rt/rostelecom_tms/dto/users/RegistrationResponseDto.java
+++ src/main/java/ru/rt/rostelecom_tms/dto/users/RegistrationResponseDto.java
@@ -3,7 +3,5 @@ package ru.rt.rostelecom_tms.dto.users;
 public record RegistrationResponseDto(
         Integer id,
         String email,
-        String username,
-        Integer projectId,
-        String projectName
+        String username
 ) {}
